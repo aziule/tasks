0b9cd0f8510e42fc6ca43c80e33c5dbc4146924e
Will <willyboy_43@hotmail.com>
2017-04-26 14:21:00 +0100

    Handle short names for commands

--- command/handler.go
+++ command/handler.go
@@ -12,10 +12,26 @@ func RegisterCommand(c Command) {
 }
 
 func HandleCommand(name string, args []string) error {
+	eligibleCommands := []Command{}
+
 	for _, c := range commands {
-		if c.GetName() == name {
-			return c.Execute(args)
+		if len(c.GetName()) >= len(name) && c.GetName()[:len(name)] == name {
+			eligibleCommands = append(eligibleCommands, c)
 		}
+	}
+
+	if len(eligibleCommands) == 1 {
+		return eligibleCommands[0].Execute(args)
+	}
+
+	if len(eligibleCommands) > 1 {
+		availableCommandsNames := []string{}
+
+		for _, c := range eligibleCommands {
+			availableCommandsNames = append(availableCommandsNames, c.GetName())
+		}
+
+		return errors.New(fmt.Sprintf("Multiple commands found: %s", availableCommandsNames))
 	}
 
 	return errors.New(fmt.Sprintf("Command \"%s\" does not exist", name))
